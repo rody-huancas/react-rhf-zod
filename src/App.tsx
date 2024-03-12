e9d1b73440df0ee781787795cfe5b744b3427f39
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-12 15:10:55 -0500

    Add: validation with zod and react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { ZodType, z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-export default App
+type FormData = {
+  firstName       : string;
+  lastName        : string;
+  email           : string;
+  age             : number;
+  password        : string;
+  confirmPassword : string;
+};
+
+const App = () => {
+  const schema: ZodType<FormData> = z.object({
+    firstName       : z.string().min(2).max(30),
+    lastName        : z.string().min(2).max(30),
+    email           : z.string().email(),
+    age             : z.number().min(18).max(80),
+    password        : z.string().min(5).max(20),
+    confirmPassword : z.string().min(5).max(20),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message : "Password do not match",
+    path    : ["confirmPassword"],
+  });
+
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const submitData = (data: FormData) => {
+    console.log('yes!', data)
+  }
+
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit(submitData)}>
+        <label>First Name:</label>
+        <input type="text" {...register('firstName') } />
+        { errors.firstName && <span>{errors.firstName.message}</span> }
+
+        <label>Last Name:</label>
+        <input type="text" {...register('lastName')} />
+        { errors.lastName && <span>{errors.lastName.message}</span> }
+
+        <label>Email:</label>
+        <input type="email" {...register('email')} />
+        { errors.email && <span>{errors.email.message}</span> }
+
+        <label>Age:</label>
+        <input type="number" {...register('age', { valueAsNumber: true })} />
+        { errors.age && <span>{errors.age.message}</span> }
+
+        <label>Password:</label>
+        <input type="password" {...register('password')} />
+        { errors.password && <span>{errors.password.message}</span> }
+
+        <label>Confirm Password:</label>
+        <input type="password" {...register('confirmPassword')} />
+        { errors.confirmPassword && <span>{errors.confirmPassword.message}</span> }
+
+
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default App;
